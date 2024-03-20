73fcee535e34cbe1d66d3b0f2993293c8351f5d2
goodperiodt <ilypsj@gmail.com>
2024-03-20 14:31:16 +0900

    clone 레파지토리 초기 설정

--- src/main/java/com/jsp/JspStarterServlet.java
+++ src/main/java/com/jsp/JspStarterServlet.java
@@ -10,12 +10,13 @@ import java.io.PrintWriter;
 
 @WebServlet(
         name = "hello",
-        urlPatterns = "/*"
+        urlPatterns = "/start" // 모든 패턴에 동작할 수 있도록 /* 로 설정, http://localhost:8181
 )
 public class JspStarterServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("Welcome to my web-app");
+        resp.sendRedirect("index.jsp");
     }
 }
